8999ea5fe56931fd0b96ca0f26e5cc8a27093e5a
jainamjhaveri <engineerjainam@gmail.com>
2018-03-28 23:18:05 +0530

    finish 7 steps of https://flutter.io/get-started/codelab/
    -  fix infinite reloading problem
    -  learned navigation
    -  handling item taps
    -  changing theme quickly
    -  using external packages

---
+++ lib/randomwords.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:myapp/randomwordsstate.dart';
+
+class RandomWords extends StatefulWidget {
+  @override
+  createState() => new RandomWordsState();
+}
